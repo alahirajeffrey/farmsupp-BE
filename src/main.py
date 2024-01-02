9c8e9ef4675a966bfe4739cbe6303f3ea0f4b702
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-02 21:03:21 +0100

    setup server

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from article.router import router as article_router
+from auth.router import router as auth_router
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+app.include_router(auth_router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(article_router , prefix="/api/v1/article", tags=["article"])
+
+
+@app.get("/")
+async def root():
+    return {"message": "Server running"}
