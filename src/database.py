1f8c5d1993f0a38ec6aed3466fdaa9504f517cc6
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-02 23:07:21 +0100

    import postgres_url from config

--- src/database.py
+++ src/database.py
@@ -2,8 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import config
 
-POSRGRES_DATABASE_URL = os.environ.get('POSTGRES_URL', "postgresql://postgres:102296@localhost:5432/farmsupp")
+
+POSRGRES_DATABASE_URL = config.get('POSTGRES_URL')
 
 engine = create_engine( POSRGRES_DATABASE_URL )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
