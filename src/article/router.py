a52e4213d0d67f5b4bd232661e69b3b8edc3c0e2
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-02 22:57:51 +0100

    article endpoints

--- src/article/router.py
+++ src/article/router.py
@@ -4,4 +4,20 @@ router = APIRouter()
 
 @router.post('', status_code=status.HTTP_201_CREATED)
 def create_article():
-    return {"message":"article createrd"}
+    return {"message":"article created"}
+
+@router.get('/{article_id}', status_code=status.HTTP_200_OK)
+def get_single_article():
+    return {"message":"article"}
+
+@router.get('/{user_id}', status_code=status.HTTP_200_OK)
+def get_all_user_articles():
+    return {"message":"article"}
+
+@router.delete('/{article_id}', status_code=status.HTTP_200_OK)
+def delete_article():
+    return {"message":"article"}
+
+@router.patch('/{article_id}', status_code=status.HTTP_200_OK)
+def update_article():
+    return {"message":"article"}
