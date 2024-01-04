f4bcff605778469b87bde11988acbb06dfff3ead
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-04 15:24:39 +0100

    added endpoints to login and change password

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, status, Depends, HTTPException
-from auth.schemas import CreateUserSchema, UserReturnSchema
+from auth import schemas
 from sqlalchemy.orm import Session
 from database import get_db
-from auth import models, utils
+from auth import utils
+import models
 
 router = APIRouter()
 
-@router.post('/register', status_code=status.HTTP_201_CREATED, response_model=UserReturnSchema)
-def register_user(data: CreateUserSchema, db: Session = Depends(get_db)):
+@router.post('/register', status_code=status.HTTP_201_CREATED, response_model= schemas.UserReturnSchema)
+async def register_user(data: schemas.CreateUserSchema, db: Session = Depends(get_db)):
     
     ## check if user exists
     user_exists = db.query(models.User).filter(models.User.mobile_number == data.mobile_number).first()
@@ -32,25 +33,66 @@ def register_user(data: CreateUserSchema, db: Session = Depends(get_db)):
     return new_user
 
 @router.post('/login', status_code=status.HTTP_200_OK)
-def login():
-    return {"message":"user signed in"}
+async def login(data: schemas.LoginSchema, db: Session = Depends(get_db)):
+    
+    ## check if user exists
+    user_exists = db.query(models.User).filter(models.User.mobile_number == data.mobile_number).first()
+    if not user_exists:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail= "User does not exist"
+        )
+    
+    ## verify password
+    if not utils.verify_password(data.password, user_exists.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password"
+        )
+    
+    access_token = utils.create_access_token(user_exists.id)
+
+    return { "access_token": access_token }
 
 @router.patch('/change-password', status_code=status.HTTP_200_OK)
-def change_password():
-    return {"message":"Change password"}
+async def change_password(
+    data: schemas.ChangePasswordSchema, 
+    db: Session = Depends(get_db), 
+    payload: dict = Depends(utils.validate_access_token)):
+
+    ## check if user exists
+    user_exists = db.query(models.User).filter(models.User.id == payload.get('sub')).first()
+    if(user_exists is None):
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= "User does not exist"
+        )
+    
+    ## verify password
+    if not utils.verify_password(data.old_password, user_exists.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect password"
+        )
+
+    ## updated user password in database
+    db.query(models.User).filter(models.User.id == payload.get('sub')).update({"password": utils.hash_password(data.new_password)})
+    db.commit()
+
+    return {"message": "Password changed successfully" }
 
 @router.post('/request-password-reset', status_code=status.HTTP_200_OK)
-def request_password_reset():
+async def request_password_reset():
     return {"message":"Password reset link sent"}
 
 @router.patch('/reset-password', status_code=status.HTTP_200_OK)
-def reset_password():
+async def reset_password():
     return {"message":"Reset password"}
 
 @router.post('/request-mobile-verification', status_code=status.HTTP_200_OK)
-def request_mobile_verification():
+async def request_mobile_verification():
     return {"message":"Mobile verification otp sent"}
 
 @router.patch('/verify-mobile', status_code=status.HTTP_200_OK)
-def verify_mobile():
+async def verify_mobile():
     return {"message":"Verify mobile"}
