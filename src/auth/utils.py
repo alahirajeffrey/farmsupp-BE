a58bc159d2cbc381acb3e85a13fc2d9f316b8978
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-04 15:23:21 +0100

    added function to validate access token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -3,6 +3,10 @@ from typing import Union, Any
 from datetime import datetime, timedelta
 from config import config
 from jose import jwt
+from fastapi.security import OAuth2PasswordBearer 
+from fastapi import HTTPException, status, Depends
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login", scheme_name="JWT")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -22,3 +26,28 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, config.get("JWT_SECRET"), "HS256")
     return encoded_jwt
+
+## validate access token
+def validate_access_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, config.get("JWT_SECRET"), algorithms="HS256")
+        sub: str = payload.get("sub")
+        if sub is None:
+            raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED, 
+            detail= "Invalid credentials"
+            )
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED, 
+            detail= "Token expired"
+            )
+    
+    except jwt.JWTError:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED, 
+            detail= "Could not validate credentials"
+            )
+
+    return payload
