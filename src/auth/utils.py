6a401e6c723dab4fdb9b0474436ba68fdd687ae0
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-04 15:15:07 +0100

    create access token function

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,8 @@
 from passlib.context import CryptContext
+from typing import Union, Any
+from datetime import datetime, timedelta
+from config import config
+from jose import jwt
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +11,14 @@ def hash_password(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+## create access token
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=30)
+    
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, config.get("JWT_SECRET"), "HS256")
+    return encoded_jwt
