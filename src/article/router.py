4102cd9cfba24eda89f8b7c4d483a2e48cb1457d
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-06 21:09:31 +0100

    code refactor

--- src/article/router.py
+++ src/article/router.py
@@ -5,6 +5,7 @@ import models
 from sqlalchemy.orm import Session
 from auth import utils as auth_utils
 from uuid import UUID
+from profile.schemas import Role
 
 router = APIRouter()
 
@@ -14,10 +15,25 @@ async def create_article(
     db: Session = Depends(get_db),
     payload: dict = Depends(auth_utils.validate_access_token)):
 
-    ## allow only farmers to create articles
+    ## convert user id from payload to UUID
+    user_id_from_token = UUID(payload.get('sub'))
+    profile = db.query(models.Profile).filter(models.Profile.user_id == user_id_from_token).first()
+
+    if profile is None:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= "Profile does not exist"
+        )
+
+    if profile.role != Role.FARMER:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail= "Only farmers can post articles"
+        )
+
 
     new_article = models.Article(
-        author_id= payload.get('sub'),
+        author_id= profile.id,
         body= data.body,
         title= data.title
     )
@@ -52,9 +68,19 @@ def get_all_user_articles(
 
     offset = (page - 1) * page_size
 
+    # user_id_from_token = UUID(payload.get('sub'))
+    # profile = db.query(models.Profile).filter(models.Profile.user_id == user_id_from_token).first()
+
+    profile_exists = db.query(models.Profile).filter(models.Profile.id == author_id).first()
+    if not profile_exists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= "Author does not exist"
+        )
+
     articles = db.query(models.Article).filter(models.Article.author_id == author_id).offset(offset).limit(page_size).all()
 
-    if len(articles) is 0:
+    if len(articles) == 0:
         raise HTTPException(
             status_code= status.HTTP_404_NOT_FOUND,
             detail= "Author has not published any articles yet"
@@ -77,8 +103,9 @@ def delete_article(
     
     ## convert user id from payload to UUID
     user_id_from_token = UUID(payload.get('sub'))
+    profile = db.query(models.Profile).filter(models.Profile.user_id == user_id_from_token).first()
      
-    if article.author_id != user_id_from_token:
+    if article.author_id != profile.id:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You cannot delete an article you do not own"
@@ -105,8 +132,9 @@ def update_article(
     
     ## convert user id from payload to UUID
     user_id_from_token = UUID(payload.get('sub'))
+    profile = db.query(models.Profile).filter(models.Profile.user_id == user_id_from_token).first()
      
-    if article.author_id != user_id_from_token:
+    if article.author_id != profile.id:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You cannot update an article you do not own"
