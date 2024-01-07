86b33c1124778711307ae168c7d1f6c19f2cb145
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-07 19:59:34 +0100

    added chat router to server

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from article.router import router as article_router
 from auth.router import router as auth_router
 from profile.router import router as profile_router
+from chat.router import router as chat_router
 
 app = FastAPI()
 
@@ -17,6 +18,7 @@ app.add_middleware(
 app.include_router(auth_router, prefix="/api/v1/auth", tags=["auth"])
 app.include_router(profile_router , prefix="/api/v1/profile", tags=["profile"])
 app.include_router(article_router , prefix="/api/v1/article", tags=["article"])
+app.include_router(chat_router , prefix="/api/v1/chat", tags=["chat"])
 
 
 @app.get("/")
