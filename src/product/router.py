6da91e92cb90c4bce9f39f76997441a922205035
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-09 08:58:57 +0100

    added endpoint to get products by id and name

--- src/product/router.py
+++ src/product/router.py
@@ -9,7 +9,7 @@ from profile.schemas import Role
 
 router = APIRouter()
 
-@router.post('', status_code=status.HTTP_201_CREATED )
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=schemas.ProductReturnSchema )
 async def create_product(
     data: schemas.CreateProductSchema,
     db: Session = Depends(get_db),
@@ -46,13 +46,39 @@ async def create_product(
 
     return new_product
 
-@router.get('/{product_id}', status_code=status.HTTP_200_OK )
-async def get_product_by_id():
-    pass
+@router.get('/{product_id}', status_code=status.HTTP_200_OK, response_model=schemas.ProductReturnSchema )
+async def get_product_by_id(
+    product_id: str,
+    db: Session = Depends(get_db),
+    payload: dict = Depends(auth_utils.validate_access_token)):
+    
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if not product:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= "Product does not exist"
+        )
+    
+    return product
 
 @router.get('/name/{name}', status_code=status.HTTP_200_OK )
-async def get_products_by_name():
-    pass
+async def get_products_by_name(
+    name: str,
+    db: Session = Depends(get_db),
+    page: int = Query(1, description="Page number", gt=0),
+    page_size: int = Query(10, description="Items per page", gt=0, le=100),
+    payload: dict = Depends(auth_utils.validate_access_token)):
+
+    offset = (page - 1) * page_size
+
+    products = db.query(models.Product).filter(models.Product.name == name)offset(offset).limit(page_size).all()
+    if len(products) == 0:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail= "Products not found"
+        )
+    
+    return products
 
 @router.patch('/upload/images', status_code=status.HTTP_200_OK )
 async def upload_product_images():
