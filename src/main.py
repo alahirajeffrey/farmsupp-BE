b93150415b3686e9d0d8f649cd147dfeedfc29a5
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-12 13:59:07 +0100

    setup routes for other package

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from auth.router import router as auth_router
 from profile.router import router as profile_router
 from chat.router import router as chat_router
 from product.router import router as product_router
+from other.router import router as other_router
 
 app = FastAPI()
 
@@ -21,6 +22,8 @@ app.include_router(profile_router , prefix="/api/v1/profile", tags=["profile"])
 app.include_router(article_router , prefix="/api/v1/article", tags=["article"])
 app.include_router(chat_router , prefix="/api/v1/chat", tags=["chat"])
 app.include_router(product_router , prefix="/api/v1/product", tags=["product"])
+app.include_router(other_router , prefix="/api/v1/other", tags=["other"])
+
 
 @app.get("/")
 async def root():
