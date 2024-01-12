60dd973dfee49f0737f6e90220e60f0d550c7eb5
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-12 12:58:11 +0100

    made request body optional

--- src/profile/schemas.py
+++ src/profile/schemas.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pydantic import BaseModel, UUID4
 from enum import Enum
+from typing import Optional
 
 class Role(str, Enum):
     FARMER= "FARMER"
@@ -8,10 +9,10 @@ class Role(str, Enum):
     USER= "USER"
 
 class UpdateProfileSchema(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    role: Role
+    first_name: Optional[str] = None 
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    role: Optional[Role] = None
 
 class ProfileReturnSchema(UpdateProfileSchema):
     created_at: datetime
